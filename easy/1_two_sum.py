f6ded0956bed4532493b0568e6f2d7d2dffd83af
infernalcodera11y <infernalwhisper666@gmail.com>
2025-04-13 16:55:42 +0530

    Update 1_two_sum.py

--- easy/1_two_sum.py
+++ easy/1_two_sum.py
@@ -1,1 +1,12 @@
 #solution for leetcode 1 - two sum
+from typing import List
+
+class Solutin:
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    num_map = {}
+    for i, num in enumerate(nums):
+      complement = target - num
+      if complement in num_map:
+        return [num_map[complement],i]
+     num_map[num] = i
+   return []
